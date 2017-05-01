e44a719fea7d3c27b9130a8586a3fa1a3817b517
Ahmed Yakout <iyakout@hotmail.com>
2017-05-02 00:00:15 +0200

    added decimal_to_hex implementaion

--- include/assembler.h
+++ include/assembler.h
@@ -6,6 +6,8 @@
 #define SIC_XE_ASSEMBLER_ASSEMBLER_H
 
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 
 namespace sic_assembler {
     const int INSTRCUTION_LENGTH = 3;
@@ -15,12 +17,18 @@ namespace sic_assembler {
     int hex_to_int(std::string hex) {
         return 0;
     }
+    std::string decimal_to_hex(int decimal) {
+        std::stringstream stream;
+        stream << std::setfill ('0') << std::setw(6)
+               << std::hex << decimal;
+        return stream.str();
+    }
     std::string to_lower(std::string str) {
-        std::transform(str.begin(), str.end(), str.begin(), std::tolower);
+        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
         return str;
     }
     std::string to_upper(std::string str) {
-        std::transform(str.begin(), str.end(), str.begin(), std::toupper);
+        std::transform(str.begin(), str.end(), str.begin(), ::toupper);
         return str;
     }
 }
