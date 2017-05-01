e93559f0dd594eb8533726909a239ae1299abbba
Marwan Tammam <marwan.01010100@gmail.com>
2017-05-01 13:40:23 +0200

    Moved singelton impl to source file

--- include/op_table.h
+++ include/op_table.h
@@ -14,15 +14,17 @@ class op_table {
 private:
     unordered_map<string, string> optab;
     op_table();
-//    static op_table& instance;
+    static op_table* instance;
 public:
     // singleton
-    static op_table& getInstance()
-    {
-        static op_table instance; // Guaranteed to be destroyed.
-        // Instantiated on first use.
-        return instance;
-    }
+    static op_table* get_instance();
+//    static op_table& getInstance()
+
+//    {
+//        static op_table instance; // Guaranteed to be destroyed.
+//         Instantiated on first use.
+//        return instance;
+//    }
     string get(string);
 };
 
--- src/op_table.cpp
+++ src/op_table.cpp
@@ -4,18 +4,20 @@
 
 #include "op_table.h"
 
+op_table* op_table::instance = nullptr;
+
 op_table::op_table() {
     optab["add"] = "18";
     optab["lda"] = "00";
     // todo
 }
 
-//op_table* op_table::getInstance() {
-//    if (op_table::instance == nullptr) {
-//        instance = new op_table();
-//    }
-//    return instance;
-//}
+op_table* op_table::get_instance() {
+    if (op_table::instance == nullptr){
+        op_table::instance = new op_table();
+    }
+    return op_table::instance;
+}
 
 string op_table::get(string key) {
     return optab[key];
