1f92e6d34109f760f60b8774726abd47addeb704
Ahmed Yakout <iyakout@hotmail.com>
2017-05-04 18:48:08 +0200

    added intermediate file writer clas

--- include/intermediate_file_writer.h
+++ include/intermediate_file_writer.h
@@ -5,4 +5,20 @@
 #ifndef SIC_ASSEMBLER_INTERMEDIATE_FILE_WRITER_H
 #define SIC_ASSEMBLER_INTERMEDIATE_FILE_WRITER_H
 
+#include <fstream>
+#include "instruction.h"
+
+class intermeditate_file_writer {
+private:
+    const std::string FILE_EXTENSION = ".i";
+    std::ofstream source_file;
+
+    void write_file_header();
+public:
+    intermeditate_file_writer(std::string);
+    ~intermeditate_file_writer();
+    void write(const instruction*);
+    void print_sym_table();
+};
+
 #endif //SIC_ASSEMBLER_INTERMEDIATE_FILE_WRITER_H
--- src/intermediate_file_writer.cpp
+++ src/intermediate_file_writer.cpp
@@ -2,3 +2,24 @@
 // Created by Ahmed Yakout on 5/4/17.
 //
 
+#include <sym_table.h>
+#include "intermediate_file_writer.h"
+
+intermeditate_file_writer::intermeditate_file_writer(std::string _path): source_file(_path) {
+    write_file_header();
+}
+
+intermeditate_file_writer::~intermeditate_file_writer() {
+    source_file.close();
+}
+
+void intermeditate_file_writer::write(const instruction * _instruction) {
+}
+
+void intermeditate_file_writer::write_file_header() {
+    source_file << "Loc" <<  "source statement" << "object code" << "error indicator";
+}
+
+void intermeditate_file_writer::print_sym_table() {
+
+}
