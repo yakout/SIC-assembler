09d05c3ea19c4caad9c16a564941cceeecdbb059
Ahmed Yakout <iyakout@hotmail.com>
2017-05-05 20:10:39 +0200

    handle both unix and dos end of line characters

--- src/file_reader.cpp
+++ src/file_reader.cpp
@@ -31,8 +31,14 @@ bool file_reader::has_next_instruction() {
         return true;
     }
     if (getline(file_reader::source_file, file_reader::buffer)) {
-        current_line_number += 5;
-        buffer = buffer.substr(0, buffer.length() - 1);
+        current_line_number++;
+        #if defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+            // UNIX STYLE OS
+            buffer = buffer.substr(0, buffer.length() - 1);
+        #elif defined(__WIN32) 
+            // __WIN32 check for both 32 and 64 so no need to check for for __WIN64
+            buffer = buffer.substr(0, buffer.length() - 2);
+        #endif
         buffer = sic_assembler::to_lower(buffer);
         buffer = sic_assembler::remove_tabs(buffer);
         return true;
